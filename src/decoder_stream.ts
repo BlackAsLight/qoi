f8e5abcfb10abf87363dc95b38ce2b15b6eaa72f
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-10-13 19:50:22 +1100

    tests: completed for `new QOIDecoderStream()`

--- src/decoder_stream.ts
+++ src/decoder_stream.ts
@@ -3,7 +3,7 @@ import type { QOIOptions } from "./types.ts";
 
 export class QOIDecoderStream
   implements TransformStream<Uint8Array, Uint8Array> {
-  #options: QOIOptions | undefined;
+  #options: QOIOptions | undefined | Error;
   #buffer: number[] = [];
   #source: ReadableStreamBYOBReader;
   #readable: ReadableStream<Uint8Array>;
@@ -27,14 +27,12 @@ export class QOIDecoderStream
     this.#writable = writable;
   }
 
-  async #read(bytes: number): Promise<Uint8Array | undefined> {
+  async #read(bytes: number): Promise<Uint8Array> {
     const { done, value } = await this.#source
       .read(new Uint8Array(bytes), { min: bytes });
-    if (done) {
-      if (value?.length) {
-        throw new RangeError("Unexpected number of bytes from readable stream");
-      }
-      return undefined;
+    if (done || value.length !== bytes) {
+      this.#source.cancel("Unexpected number of bytes from readable stream");
+      throw new RangeError("Unexpected number of bytes from readable stream");
     }
     this.#buffer.push(...value);
     return Uint8Array.from(this.#buffer.splice(0, this.#buffer.length - 8));
@@ -45,12 +43,17 @@ export class QOIDecoderStream
     while (this.#options == undefined) {
       await new Promise((a) => setTimeout(a, 0));
     }
+    if (this.#options instanceof Error) {
+      this.#source.cancel(this.#options.message);
+      throw this.#options;
+    }
 
     // Load Buffer
     {
       const { done, value } = await this.#source
         .read(new Uint8Array(8), { min: 8 });
       if (done || value.length !== 8) {
+        this.#source.cancel("QOI stream was too short to be valid");
         throw new RangeError("QOI stream was too short to be valid");
       }
       this.#buffer.push(...value);
@@ -64,52 +67,33 @@ export class QOIDecoderStream
       // Premature Exit
       if ([0, 0, 0, 0, 0, 0, 0, 1].every((x, i) => x === this.#buffer[i])) {
         if (count !== this.#options.width * this.#options.height) {
+          this.#source.cancel(
+            `QOI stream received exit code, but pixels (${count}) decoded does not match width * height (${
+              this.#options.width * this.#options.height
+            }) in header`,
+          );
           throw new RangeError(
             `QOI stream received exit code, but pixels (${count}) decoded does not match width * height (${
               this.#options.width * this.#options.height
             }) in header`,
           );
         }
-        await this.#source.cancel("QOI stream finished prematurely");
+        await this.#source.cancel("QOI stream finished");
         return;
       }
 
       const value = await this.#read(1);
-      // Natural Exit
-      if (value == undefined) {
-        if (![0, 0, 0, 0, 0, 0, 0, 1].every((x, i) => x === this.#buffer[i])) {
-          throw new RangeError("QOI stream has invalid exit code");
-        }
-        if (count !== this.#options.width * this.#options.height) {
-          throw new RangeError(
-            `Pixels (${count}) decoded does not match width * height (${
-              this.#options.width * this.#options.height
-            })`,
-          );
-        }
-        return;
-      }
       ++count;
 
       if (value[0] === 0b11111111) {
         // QOI_OP_RGBA
         const pixel = await this.#read(4);
-        if (pixel == undefined) {
-          throw new RangeError(
-            "Unexpected number of bytes from readable stream",
-          );
-        }
         previousPixel = pixel;
         seenPixels[this.#index(previousPixel)] = previousPixel;
         yield previousPixel.slice();
       } else if (value[0] === 0b11111110) {
         // QOI_OP_RGB
         const pixel = await this.#read(3);
-        if (pixel == undefined) {
-          throw new RangeError(
-            "Unexpected number of bytes from readable stream",
-          );
-        }
         previousPixel = new Uint8Array([...pixel, 255]);
         seenPixels[this.#index(previousPixel)] = previousPixel;
         yield previousPixel.slice();
@@ -133,11 +117,6 @@ export class QOIDecoderStream
           case 2: {
             // QOI_OP_LUMA
             const next = await this.#read(1);
-            if (next == undefined) {
-              throw new RangeError(
-                "Unexpected number of bytes from readable stream",
-              );
-            }
             const greenDiff = (value[0] & 0b00_111111) - 32;
             previousPixel = new Uint8Array([
               (next[0] >> 4) + greenDiff + previousPixel[0] - 8,
@@ -162,25 +141,24 @@ export class QOIDecoderStream
   async #header(): Promise<void> {
     const { done, value } = await this.#source
       .read(new Uint8Array(14), { min: 14 });
+
     if (done || value.length !== 14) {
-      throw new RangeError("QOI stream was too short to be valid");
+      this.#options = new RangeError("QOI stream was too short to be valid");
+    } else if (![113, 111, 105, 102].every((x, i) => x === value[i])) {
+      this.#options = new TypeError("QOI stream had invalid magic number");
+    } else if (value[12] !== 3 && value[12] !== 4) {
+      this.#options = new TypeError("QOI stream had invalid channels");
+    } else if (value[13] !== 0 && value[13] !== 1) {
+      this.#options = new TypeError("QOI stream had invalid colorspace");
+    } else {
+      const view = new DataView(value!.buffer);
+      this.#options = {
+        width: view.getUint32(4),
+        height: view.getUint32(8),
+        channels: value![12] === 3 ? "rgb" : "rgba",
+        colorspace: value![13] as 0,
+      };
     }
-    if (![113, 111, 105, 102].every((x, i) => x === value[i])) {
-      throw new TypeError("QOI stream had invalid magic number");
-    }
-    if (value[12] !== 3 && value[12] !== 4) {
-      throw new TypeError("QOI stream had invalid channels");
-    }
-    if (value[13] !== 0 && value[13] !== 1) {
-      throw new TypeError("QOI stream had invalid colorspace");
-    }
-    const view = new DataView(value.buffer);
-    this.#options = {
-      width: view.getUint32(4),
-      height: view.getUint32(8),
-      channels: value[12] === 3 ? "rgb" : "rgba",
-      colorspace: value[13],
-    };
   }
 
   get options(): Promise<QOIOptions> | QOIOptions {
@@ -189,9 +167,11 @@ export class QOIDecoderStream
         while (this.#options == undefined) {
           await new Promise((a) => setTimeout(a, 0));
         }
+        if (this.#options instanceof Error) throw this.#options;
         return this.#options;
       })();
-    } else return this.#options;
+    } else if (this.#options instanceof Error) throw this.#options;
+    else return this.#options;
   }
 
   #index(a: Uint8Array): number {
--- src/decoder_stream_test.ts
+++ src/decoder_stream_test.ts
@@ -8,7 +8,7 @@ Deno.test("QOIDecoderStream() correctly decoding header", async () => {
   const decoderStream = new QOIDecoderStream();
 
   const readable = ReadableStream
-    .from([new Uint8Array([0, 0, 0, 255])])
+    .from([new Uint8Array([128, 128, 128, 255])])
     .pipeThrough(
       new QOIEncoderStream({
         width: 1,
@@ -26,6 +26,31 @@ Deno.test("QOIDecoderStream() correctly decoding header", async () => {
     colorspace: 0,
   });
   await readable.cancel();
+});
+
+Deno.test("QOIDecoderStream() getting options after decoding body", async () => {
+  const decoderStream = new QOIDecoderStream();
+
+  const readable = ReadableStream
+    .from([new Uint8Array([128, 128, 128, 255])])
+    .pipeThrough(
+      new QOIEncoderStream({
+        width: 1,
+        height: 1,
+        channels: "rgb",
+        colorspace: 0,
+      }),
+    )
+    .pipeThrough(decoderStream);
+
+  // deno-lint-ignore no-empty
+  for await (const _ of readable) {}
+  assertEquals(await decoderStream.options, {
+    width: 1,
+    height: 1,
+    channels: "rgb",
+    colorspace: 0,
+  });
 });
 
 Deno.test("QOIDecoderStream() correctly decoding QOI_OP_RGBA", async () => {
@@ -176,8 +201,93 @@ Deno.test("QOIDecoderStream() correctly decoding QOI_OP_RUN", async () => {
   );
 });
 
+Deno.test("QOIDecoderStream() rejecting header due to shortness", async () => {
+  const decoderStream = new QOIDecoderStream();
+
+  await assertRejects(
+    async () => {
+      for await (
+        const _ of ReadableStream
+          .from([new Uint8Array(13)])
+          .pipeThrough(decoderStream)
+        // deno-lint-ignore no-empty
+      ) {}
+    },
+    RangeError,
+    "QOI stream was too short to be valid",
+  );
+
+  await assertRejects(
+    async () => {
+      await decoderStream.options;
+    },
+    RangeError,
+    "QOI stream was too short to be valid",
+  );
+});
+
+Deno.test("QOIDecoderStream() rejecting header due to magic number", async () => {
+  const decoderStream = new QOIDecoderStream();
+
+  const options = decoderStream.options;
+  await assertRejects(
+    async () => {
+      for await (
+        const _ of ReadableStream
+          .from([new Uint8Array(14)])
+          .pipeThrough(decoderStream)
+        // deno-lint-ignore no-empty
+      ) {}
+    },
+    TypeError,
+    "QOI stream had invalid magic number",
+  );
+
+  await assertRejects(
+    async () => {
+      await options;
+    },
+    TypeError,
+    "QOI stream had invalid magic number",
+  );
+});
+
+Deno.test("QOIDecoderStream() rejecting header due to channels", async () => {
+  await assertRejects(
+    async () => {
+      for await (
+        const _ of ReadableStream
+          .from([
+            new Uint8Array([113, 111, 105, 102, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]),
+          ])
+          .pipeThrough(new QOIDecoderStream())
+        // deno-lint-ignore no-empty
+      ) {}
+    },
+    TypeError,
+    "QOI stream had invalid channels",
+  );
+});
+
+Deno.test("QOIDecoderStream() rejecting header due to colorspace", async () => {
+  await assertRejects(
+    async () => {
+      for await (
+        const _ of ReadableStream
+          .from([
+            new Uint8Array([113, 111, 105, 102, 0, 0, 0, 1, 0, 0, 0, 1, 3, 2]),
+          ])
+          .pipeThrough(new QOIDecoderStream())
+        // deno-lint-ignore no-empty
+      ) {}
+    },
+    TypeError,
+    "QOI stream had invalid colorspace",
+  );
+});
+
 Deno.test("QOIDecoderStream() rejecting invalid premature exit", async () => {
-  const header = (await toBytes(
+  const bytes = await toBytes(
     ReadableStream
       .from([new Uint8Array([128, 128, 128, 255])])
       .pipeThrough(
@@ -188,19 +298,75 @@ Deno.test("QOIDecoderStream() rejecting invalid premature exit", async () => {
           colorspace: 0,
         }),
       ),
-  )).slice(0, 14);
+  );
 
   await assertRejects(
     async () => {
-      const decoderStream = new QOIDecoderStream();
       for await (
         const _ of ReadableStream
-          .from([header, new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1])])
-          .pipeThrough(decoderStream)
+          .from([bytes.slice(0, 14), bytes.slice(-8)])
+          .pipeThrough(new QOIDecoderStream())
         // deno-lint-ignore no-empty
       ) {}
     },
     RangeError,
     "QOI stream received exit code, but pixels (0) decoded does not match width * height (1) in header",
+  );
+});
+
+Deno.test("QOIDecoderStream() rejecting invalid buffer loading", async () => {
+  const bytes = (await toBytes(
+    ReadableStream
+      .from([new Uint8Array([128, 128, 128, 255])])
+      .pipeThrough(
+        new QOIEncoderStream({
+          width: 1,
+          height: 1,
+          channels: "rgb",
+          colorspace: 0,
+        }),
+      ),
+  )).slice(0, 15);
+
+  await assertRejects(
+    async () => {
+      for await (
+        const _ of ReadableStream
+          .from([bytes])
+          .pipeThrough(new QOIDecoderStream())
+        // deno-lint-ignore no-empty
+      ) {}
+    },
+    RangeError,
+    "QOI stream was too short to be valid",
+  );
+});
+
+Deno.test("QOIDecodedStream() rejecting invalid length", async () => {
+  const bytes = await toBytes(
+    ReadableStream
+      .from([new Uint8Array([128, 128, 128, 255])])
+      .pipeThrough(
+        new QOIEncoderStream({
+          width: 1,
+          height: 1,
+          channels: "rgb",
+          colorspace: 0,
+        }),
+      ),
+  );
+  bytes[bytes.length - 1] = 0;
+
+  await assertRejects(
+    async () => {
+      for await (
+        const _ of ReadableStream
+          .from([bytes])
+          .pipeThrough(new QOIDecoderStream())
+        // deno-lint-ignore no-empty
+      ) {}
+    },
+    RangeError,
+    "Unexpected number of bytes from readable stream",
   );
 });
