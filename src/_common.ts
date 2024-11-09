4b245544bf08e306ddb3aa9c9e68281d3b62ea62
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2024-11-09 23:21:28 +1100

    refactor: extract core decoding to `_common.ts`

--- src/_common.ts
+++ src/_common.ts
@@ -106,3 +106,79 @@ export function createEncoder(isRGB: boolean): (
     return { i, o };
   };
 }
+
+export function createDecoder(): (
+  input: Uint8Array,
+  i: number,
+  o: number,
+) => { i: number; o: number; c: number; isEnd: boolean } {
+  const previousPixel = new Uint8Array([0, 0, 0, 255]);
+  const seenPixels: Uint8Array[] = new Array(64)
+    .fill(0)
+    .map((_) => new Uint8Array([0, 0, 0, 0]));
+  return function (
+    data: Uint8Array,
+    i: number,
+    o: number,
+  ): { i: number; o: number; c: number; isEnd: boolean } {
+    let c = 0;
+    for (; i <= data.length - 8; o += 4) {
+      if ([0, 0, 0, 0, 0, 0, 0, 1].every((x, j) => x === data[i + j])) {
+        return { i, o, c, isEnd: true };
+      }
+      ++c;
+
+      if (data[i] === 0b11111111) {
+        // QOI_OP_RGBA
+        previousPixel.set(data.subarray(i + 1, i + 5));
+        seenPixels[calcIndex(previousPixel, false)].set(previousPixel);
+        data.set(previousPixel, o);
+        i += 5;
+      } else if (data[i] === 0b11111110) {
+        // QOI_OP_RGB
+        previousPixel.set(data.subarray(i + 1, i + 4));
+        previousPixel[3] = 255;
+        seenPixels[calcIndex(previousPixel, true)].set(previousPixel);
+        data.set(previousPixel, o);
+        i += 4;
+      } else {
+        switch (data[i] >> 6) {
+          case 0:
+            // QOI_OP_INDEX
+            previousPixel.set(seenPixels[data[i++] & 0b00111111]);
+            data.set(previousPixel, o);
+            break;
+          case 1:
+            // QOI_OP_DIFF
+            previousPixel[0] += (data[i] >> 4 & 0b11) - 2;
+            previousPixel[1] += (data[i] >> 2 & 0b11) - 2;
+            previousPixel[2] += (data[i++] & 0b11) - 2;
+            seenPixels[calcIndex(previousPixel, false)].set(previousPixel);
+            data.set(previousPixel, o);
+            break;
+          case 2: {
+            // QOI_OP_LUMA
+            const greenDiff = (data[i] & 0b00111111) - 32;
+            previousPixel[0] += (data[++i] >> 4) + greenDiff - 8;
+            previousPixel[1] += greenDiff;
+            previousPixel[2] += (data[i++] & 0b00001111) + greenDiff - 8;
+            seenPixels[calcIndex(previousPixel, false)].set(previousPixel);
+            data.set(previousPixel, o);
+            break;
+          }
+          default: { // 3
+            // QOI_OP_RUN
+            const run = data[i++] & 0b00111111;
+            c += run;
+            for (let j = 0; j < run; ++j) {
+              data.set(previousPixel, o);
+              o += 4;
+            }
+            data.set(previousPixel, o);
+          }
+        }
+      }
+    }
+    return { i, o, c, isEnd: false };
+  };
+}
--- src/decode.ts
+++ src/decode.ts
@@ -1,4 +1,4 @@
-import { calcIndex } from "./_common.ts";
+import { createDecoder } from "./_common.ts";
 import type { QOIOptions } from "./types.ts";
 
 /**
@@ -56,83 +56,20 @@ export function decodeQOI(
     8;
   // deno-lint-ignore no-explicit-any
   const output = new Uint8Array((input.buffer as any).transfer(maxSize));
-  if (maxSize > originalSize) {
-    output.set(output.subarray(0, originalSize), maxSize - originalSize);
-  }
+  output.set(output.subarray(0, originalSize), maxSize - originalSize);
 
-  let i = 14 + maxSize - originalSize;
-  let o = 0;
-  let count = 0;
-  const previousPixel = new Uint8Array([0, 0, 0, 255]);
-  const seenPixels: Uint8Array[] = new Array(64)
-    .fill(0)
-    .map((_) => new Uint8Array(4));
-  while (i <= output.length - 8) {
-    if ([0, 0, 0, 0, 0, 0, 0, 1].every((x, j) => x === output[i + j])) {
-      if (count !== header.width * header.height) {
-        throw new RangeError(
-          `QOI input received exit code, but pixels (${count}) decoded does not match width * height (${
-            header.width * header.height
-          }) in header`,
-        );
-      }
-      return {
-        header,
-        body: new Uint8Array(
-          // deno-lint-ignore no-explicit-any
-          (output.buffer as any).transfer(4 * header.width * header.height),
-        ),
-      };
+  const decoder = createDecoder();
+  const { o, c, isEnd } = decoder(output, maxSize - originalSize + 14, 0);
+  if (isEnd) {
+    if (c !== header.width * header.height) {
+      throw new RangeError(
+        `QOI input received exit code, but pixels (${c}) decoded does not match width * height (${
+          header.width * header.height
+        }) in header`,
+      );
     }
-    ++count;
-
-    if (output[i] === 0b11111111) {
-      // QOI_OP_RGBA
-      previousPixel.set(output.subarray(i + 1, i + 5));
-      seenPixels[calcIndex(previousPixel, false)].set(previousPixel);
-      output.set(previousPixel, o++ * 4);
-      i += 5;
-    } else if (output[i] === 0b11111110) {
-      // QOI_OP_RGB
-      previousPixel.set(output.subarray(i + 1, i + 4));
-      previousPixel[3] = 255;
-      seenPixels[calcIndex(previousPixel, true)].set(previousPixel);
-      output.set(previousPixel, o++ * 4);
-      i += 4;
-    } else {
-      switch (output[i] >> 6) {
-        case 0:
-          // QOI_OP_INDEX
-          previousPixel.set(seenPixels[output[i++] & 0b00111111]);
-          output.set(previousPixel, o++ * 4);
-          break;
-        case 1:
-          // QOI_OP_DIFF
-          previousPixel[0] += (output[i] >> 4 & 0b11) - 2;
-          previousPixel[1] += (output[i] >> 2 & 0b11) - 2;
-          previousPixel[2] += (output[i++] & 0b11) - 2;
-          seenPixels[calcIndex(previousPixel, false)].set(previousPixel);
-          output.set(previousPixel, o++ * 4);
-          break;
-        case 2: {
-          // QOI_OP_LUMA
-          const greenDiff = (output[i] & 0b00111111) - 32;
-          previousPixel[0] += (output[++i] >> 4) + greenDiff - 8;
-          previousPixel[1] += greenDiff;
-          previousPixel[2] += (output[i++] & 0b00001111) + greenDiff - 8;
-          seenPixels[calcIndex(previousPixel, false)].set(previousPixel);
-          output.set(previousPixel, o++ * 4);
-          break;
-        }
-        default: { // 3
-          // QOI_OP_RUN
-          const run = output[i++] & 0b00111111;
-          count += run;
-          for (let j = 0; j < run; ++j) output.set(previousPixel, o++ * 4);
-          output.set(previousPixel, o++ * 4);
-        }
-      }
-    }
+    // deno-lint-ignore no-explicit-any
+    return { header, body: new Uint8Array((output.buffer as any).transfer(o)) };
   }
   throw new RangeError("Expected more bytes from input");
 }
